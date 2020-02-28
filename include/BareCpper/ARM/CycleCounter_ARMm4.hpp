cd78f636c56ec99d502bf74ff4006dbed53faf88
Craig Hutchinson <54269136+CraigHutchinson@users.noreply.github.com>
2020-02-28 16:31:45 +0000

    Added common CycleTimer for cortex DWT implementations

---
+++ include/BareCpper/ARM/CycleCounter_ARMm4.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#ifndef BARECPPER_CYCLETIMER_HPP
+#  error "Include <BareCpper/CycleCounter.h> instead of this file."
+#endif
+
+
+namespace BareCpper
+{
+    inline constexpr uint32_t CycleCounter::usToTicks(const uint32_t timeUs)
+    {
+        /// @todo Hard coded clock frequency!!
+        return timeUs * 64U;
+    }
+    
+    inline CycleCounter::CycleCounter()
+    {
+        initialise();
+    }
+
+    inline bool CycleCounter::initialise()
+    {
+        CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk; //< Enable DWT
+        start();
+        return true;
+    }
+
+    inline void CycleCounter::stop()
+    {
+        DWT->CTRL &= ~DWT_CTRL_CYCCNTENA_Msk; //< Disable cycle counter
+    }
+
+    inline void CycleCounter::reset()
+    {
+        DWT->CYCCNT = 0; //< Reset counter
+    }
+
+    /** @note 5-ticks on nrf52 */
+    inline void CycleCounter::start()
+    {
+        DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk; //< Enable cycle counter
+    }
+
+    inline uint32_t CycleCounter::count()
+    {
+        return DWT->CYCCNT;
+    }
+
+} //END: BareCpper
