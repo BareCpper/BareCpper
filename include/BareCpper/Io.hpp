65accee3eeec6d06cdac3ec02014c3c4161576a2
Craig Hutchinson <54269136+CraigHutchinson@users.noreply.github.com>
2020-03-18 14:00:10 +0000

    Fixed descriptor constness

--- include/BareCpper/Io.hpp
+++ include/BareCpper/Io.hpp
@@ -1,6 +1,7 @@
 #ifndef BARECPPER_IO_HPP
 #define BARECPPER_IO_HPP
 
+#include <cassert> //< assert
 
 namespace BareCpper
 {
@@ -8,12 +9,14 @@ namespace BareCpper
     struct IoDescriptor;
 
     /** IO read function prototype
+     * @return Bytes read. 
+     * @note If Bytes-read > bufferLength this indicates insufficient buffer storage was given to complete a read of fixed-size.
      */
-    typedef int32_t (*ReadFn) (IoDescriptor& descriptor, uint8_t* const buffer, const uint16_t bufferLength);
+    typedef int32_t (*ReadFn) (const IoDescriptor& descriptor, uint8_t* const buffer, const uint16_t bufferLength);
     
     /** IO write function prototype
      */
-    typedef int32_t (*WriteFn) (IoDescriptor& descriptor, const uint8_t* const buffer, const uint16_t bufferLength);
+    typedef int32_t (*WriteFn) (const IoDescriptor& descriptor, const uint8_t* const buffer, const uint16_t bufferLength);
 
     struct IoDescriptor {
         ReadFn  read; ///< The read function pointer
@@ -22,17 +25,17 @@ namespace BareCpper
 
     /** Read from IO device
     */
-    inline int32_t read(IoDescriptor& descriptor, uint8_t* const buffer, const uint16_t bufferLength) 
+    inline int32_t read( const IoDescriptor& descriptor, uint8_t* const buffer, const uint16_t bufferLength) 
     {
-        ASSERT(buffer);
+        assert(buffer);
         return descriptor.read(descriptor, buffer, bufferLength);
     }
 
     /** Write to IO device 
     */
-    inline int32_t write( IoDescriptor& descriptor, const uint8_t* const buffer, const uint16_t bufferLength)
+    inline int32_t write(const IoDescriptor& descriptor, const uint8_t* const buffer, const uint16_t bufferLength)
     {
-        ASSERT(buffer);
+        assert(buffer);
         return descriptor.write(descriptor, buffer, bufferLength);
     }
 
