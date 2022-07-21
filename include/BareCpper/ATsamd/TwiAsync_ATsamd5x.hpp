ba2e7ab9d5e6c9fd615018b02e171c978e94a4d4
Bojan Sofronievski <bojan.drago@gmail.com>
2022-07-21 13:49:40 +0200

    Hacky fix to prevent multiple sercom handlers definitions.

--- include/BareCpper/ATsamd/TwiAsync_ATsamd5x.hpp
+++ include/BareCpper/ATsamd/TwiAsync_ATsamd5x.hpp
@@ -644,21 +644,3 @@ namespace BareCpper
 
     inline TwiAsync* TwiAsync::sercom_s[SERCOM_INST_NUM] = {};
 } //END: BareCpper
-
-// multiple inclusions problem
-extern "C" void SERCOM2_0_Handler( void )
-{
-    BareCpper::TwiAsync::SERCOM2_Handler();
-}
-extern "C" void SERCOM2_1_Handler( void )
-{
-    BareCpper::TwiAsync::SERCOM2_Handler();
-}
-extern "C" void SERCOM2_2_Handler( void )
-{
-    BareCpper::TwiAsync::SERCOM2_Handler();
-}
-extern "C" void SERCOM2_3_Handler( void )
-{
-    BareCpper::TwiAsync::SERCOM2_Handler();
-}
