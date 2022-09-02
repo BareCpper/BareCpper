8ef5d68c8e4cd5259e5d999b459c0b264a6901ee
Bojan Sofronievski <bojan.drago@gmail.com>
2022-09-02 12:12:05 +0200

    Update TemperatureSensor.hpp

--- include/BareCpper/TemperatureSensor.hpp
+++ include/BareCpper/TemperatureSensor.hpp
@@ -17,5 +17,12 @@ namespace BareCpper
 		 * @return TemperatureT The temperature in deg C.
 		 */
 		virtual TemperatureT getTemperature() = 0;
+
+		/** Start an async read returning true on subsequent call after completion
+		 * @remark The return value must be polled and will only be `true` after async read completes. Subsequenct call will initiate a new async read. 
+		 * @param temperature Reading when return==true
+		 * @return True when async read completes, otherwise false
+		*/
+		virtual bool getTemperatureAsync(TemperatureT& temperature) = 0;
 	};
 }
