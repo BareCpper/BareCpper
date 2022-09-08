427b2372168712b245153ee9241012ac4421d323
Bojan Sofronievski <bojan.drago@gmail.com>
2022-09-08 15:58:22 +0200

    Added header version check for SERCOM2 handler.

--- src/BareCpper/ATsamd/TwiAsync_ATsamd5x.cpp
+++ src/BareCpper/ATsamd/TwiAsync_ATsamd5x.cpp
@@ -4,6 +4,12 @@
 #include "BareCpper/ATsamd/TwiAsync_ATsamd5x.hpp"
 #endif
 
+#if __has_include(<component-version.h>) 
+#include <component-version.h> //< Atmel SAM version defines
+#else
+#define COMPONENT_VERSION_MAJOR 1 //< Arduino has v1 headers
+#endif
+
 extern "C"
 {
   void SERCOM2_0_Handler(void)
@@ -20,8 +26,11 @@ extern "C"
   {
     BareCpper::TwiAsync::SERCOM2_Handler();
   }
-
+#if COMPONENT_VERSION_MAJOR == 1
   void SERCOM2_3_Handler(void)
+#elif COMPONENT_VERSION_MAJOR == 2
+  void SERCOM2_OTHER_Handler(void)
+#endif
   {
     BareCpper::TwiAsync::SERCOM2_Handler();
   }
